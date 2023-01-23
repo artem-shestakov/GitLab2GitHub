afc782b368088c1086fea3bd2e1ce1d2c7255318
Артем Шестаков <ashestakov@iMac-Artem.local>
2023-01-23 15:10:13 +0300

    feat: Add create repository command

--- gitlab2github/github/repo.py
+++ gitlab2github/github/repo.py
@@ -7,9 +7,9 @@ from rich.table import Table
 
 app = typer.Typer()
 
-# @app.command()
-# def check(repo_name: str):
-#     asyncio.run(utils.get_repo(repo_name))
+@app.command()
+def check(repo_name: str):
+    asyncio.run(utils.check_repo(repo_name))
 
 @app.command()
 def create(repo_name: str):
